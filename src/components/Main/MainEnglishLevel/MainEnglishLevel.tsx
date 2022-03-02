6ffcd766d51bd7cc702ee9eca34d49810d027c91
pain4metoo <sonny30021@gmail.com>
2022-03-02 17:58:38 +0300

    feat: change design section english-level

--- src/components/Main/MainEnglishLevel/MainEnglishLevel.tsx
+++ src/components/Main/MainEnglishLevel/MainEnglishLevel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./MainEnglishLevel.module.scss";
 // import { Routes, Route } from "react-router-dom";
+import MainWelcomeBg from "../MainWelcome/MainWelcomeBg/MainWelcomeBg";
 import levelImg1 from "../../../assets/svg/level-1.svg";
 import levelImg2 from "../../../assets/svg/level-2.svg";
 import levelImg3 from "../../../assets/svg/level-3.svg";
@@ -8,45 +9,47 @@ import levelImg3 from "../../../assets/svg/level-3.svg";
 const MainEnglishLevel = () => {
   return (
     <section className={styles.english}>
-      <div className={styles.container}>
-        <div className={styles.english_inner}>
-          <h2 className={styles.english_text}>
-            Какой у вас уровень английского?
-          </h2>
-          <div className={styles.english_cards}>
-            <div className={`${styles.english_card} ${styles.english_card_1}`}>
-              <img className={styles.english_img} src={levelImg1} alt="card" />
-              <p className={styles.english_level}>Базовый</p>
-              <p className={styles.english_description}>
-                Study smarter, get better grades, and fulfill your true
-                potential by learning in the way that makes sense for you.
-              </p>
-              <button className={styles.english_btn} type="button">
-                Выбрать
-              </button>
-            </div>
-            <div className={`${styles.english_card} ${styles.english_card_2}`}>
-              <img className={styles.english_img} src={levelImg2} alt="card" />
-              <p className={styles.english_level}>Базовый</p>
-              <p className={styles.english_description}>
-                Study smarter, get better grades, and fulfill your true
-                potential by learning in the way that makes sense for you.
-              </p>
-              <button className={styles.english_btn} type="button">
-                Выбрать
-              </button>
-            </div>
-            <div className={`${styles.english_card} ${styles.english_card_3}`}>
-              <img className={styles.english_img} src={levelImg3} alt="card" />
-              <p className={styles.english_level}>Базовый</p>
-              <p className={styles.english_description}>
-                Study smarter, get better grades, and fulfill your true
-                potential by learning in the way that makes sense for you.
-              </p>
-              <button className={styles.english_btn} type="button">
-                Выбрать
-              </button>
-            </div>
+      <MainWelcomeBg />
+
+      <div className={styles.english_inner}>
+        <h2 className={styles.english_text}>
+          Какой у вас уровень английского?
+        </h2>
+        <div className={styles.english_cards}>
+          <div className={`${styles.english_card} ${styles.english_card_1}`}>
+            <img className={styles.english_img} src={levelImg1} alt="card" />
+            <p className={styles.english_level}>Базовый</p>
+            <p className={styles.english_description}>
+              Study smarter, get better grades, and fulfill your true potential
+              by learning in the way that makes sense for you.
+            </p>
+            <button className={styles.english_btn} type="button">
+              Выбрать
+            </button>
+          </div>
+
+          <div className={`${styles.english_card} ${styles.english_card_2}`}>
+            <img className={styles.english_img} src={levelImg2} alt="card" />
+            <p className={styles.english_level}>Средний</p>
+            <p className={styles.english_description}>
+              Study smarter, get better grades, and fulfill your true potential
+              by learning in the way that makes sense for you.
+            </p>
+            <button className={styles.english_btn} type="button">
+              Выбрать
+            </button>
+          </div>
+
+          <div className={`${styles.english_card} ${styles.english_card_3}`}>
+            <img className={styles.english_img} src={levelImg3} alt="card" />
+            <p className={styles.english_level}>Продвинутый</p>
+            <p className={styles.english_description}>
+              Study smarter, get better grades, and fulfill your true potential
+              by learning in the way that makes sense for you.
+            </p>
+            <button className={styles.english_btn} type="button">
+              Выбрать
+            </button>
           </div>
         </div>
       </div>
